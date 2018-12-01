6b257884b0f7604545203d18556974bd2d07531f
Ronnie Miksch <fakepreferences@gmail.com>
2018-12-01 12:45:21 -0800

    Camera class helper methods

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 from scipy import linalg
+import numpy as np
 
 class Camera(object):
   # using the pin-hole camera model
@@ -19,3 +20,35 @@ class Camera(object):
       x[i] /= x[2]
 
     return x
+
+  def rotation_matrix(self, a):
+    # 3d rotation matrix around axis vector and
+    R = np.eye(4)
+    R[:3, :3] = linalg.expm([[0,-a[2],a[1]],[a[2],0,-a[0]],[-a[1],a[0],0]])
+    return R
+
+  def factor(self):
+    # factorize camera matrix into K, R, T, as P = K[R|T]
+
+    K, R = linalg.rq(self.P[:, :3])
+
+    T = np.diag(np.sign(np.diag(K)))
+
+    if linalg.det(T) < 0:
+      T[1,1] *= -1
+
+    self.K = dot(K, T)
+    self.R = dot(T, R)
+    self.T = dot(linalg.inv(self.K), self.P[:, 3])
+
+    return self.K, self.R, self.T
+
+  def center(self):
+    # compute camera center
+
+    if self.C is not None:
+      return self.C
+
+    self.factor()
+    self.C = -dot(self.R.T, self.T)
+    return self.C
